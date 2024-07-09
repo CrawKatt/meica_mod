7f0a3d04de11a1f07363eb6c864fb7ced3c6ca84
crawkatt <man849916@gmail.com>
2024-07-09 14:09:47 -0400

    Añadiendo nuevos generadores de datos para dropeo de Loot por Entidad

--- src/main/java/com/example/examplemod/datagen/ModLootTableProvider.java
+++ src/main/java/com/example/examplemod/datagen/ModLootTableProvider.java
@@ -11,7 +11,8 @@ import java.util.Set;
 public class ModLootTableProvider {
     public static LootTableProvider create(PackOutput output) {
         return new LootTableProvider(output, Set.of(), List.of(
-                new LootTableProvider.SubProviderEntry(ModBlockLootTables::new, LootContextParamSets.BLOCK)
+                new LootTableProvider.SubProviderEntry(ModBlockLootTables::new, LootContextParamSets.BLOCK),
+                new LootTableProvider.SubProviderEntry(ModEntityLootTables::new, LootContextParamSets.ENTITY)
         ));
     }
 }
