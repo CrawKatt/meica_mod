f8f269036b2a96cbafcd57363ca3f76902eccbe6
crawkatt <man849916@gmail.com>
2024-08-09 18:06:23 -0400

    Se ha simplificado el código en el método `makeBow`

--- src/main/java/com/crawkatt/meicamod/item/ModItemProperties.java
+++ src/main/java/com/crawkatt/meicamod/item/ModItemProperties.java
@@ -10,13 +10,8 @@ public class ModItemProperties {
     }
 
     private static void makeBow(Item item) {
-        ItemProperties.register(item, new ResourceLocation("pull"), (itemStack, level, livingEntity, p_174638_) -> {
-            if (livingEntity == null) {
-                return 0.0F;
-            } else {
-                return livingEntity.getUseItem() != itemStack ? 0.0F : (float)(itemStack.getUseDuration() - livingEntity.getUseItemRemainingTicks()) / 20.0F;
-            }
-        });
+        ItemProperties.register(item, new ResourceLocation("pull"), (itemStack, level, livingEntity, p_174638_) ->
+                livingEntity == null ? 0.0F : livingEntity.getUseItem() != itemStack ? 0.0F : (float) (itemStack.getUseDuration() - livingEntity.getUseItemRemainingTicks()) / 20.0F);
 
         ItemProperties.register(item, new ResourceLocation("pulling"), (itemStack, level, livingEntity, p_174633_) ->
                 livingEntity != null && livingEntity.isUsingItem() && livingEntity.getUseItem() == itemStack ? 1.0F : 0.0F);
