b65828d5b7f0da96a63d24858c59f898b2a52002
CrawKatt <man849916@gmail.com>
2025-03-03 23:21:15 -0300

    Corrige un bug que impide la generación de minerales de Brotenita en el mundo

---
+++ src/main/java/com/crawkatt/meicamod/worldgen/gen/ModOreGeneration.java
@@ -0,0 +1,13 @@
+package com.crawkatt.meicamod.worldgen.gen;
+
+import com.crawkatt.meicamod.worldgen.ModPlacedFeatures;
+import net.fabricmc.fabric.api.biome.v1.BiomeModifications;
+import net.fabricmc.fabric.api.biome.v1.BiomeSelectors;
+import net.minecraft.world.gen.GenerationStep;
+
+public class ModOreGeneration {
+    public static void generateOres() {
+        BiomeModifications.addFeature(BiomeSelectors.foundInOverworld(),
+                GenerationStep.Feature.UNDERGROUND_ORES, ModPlacedFeatures.BROTENITA_ORE_PLACED_KEY);
+    }
+}
