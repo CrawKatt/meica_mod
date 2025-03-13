fdecaf4b39021eccc118a51ccf026b7494609d57
CrawKatt <man849916@gmail.com>
2025-03-13 14:07:12 -0300

    Corrige un Bug que otorga visión nocturna infinita si el jugador tiene una armadura de Brotenita completa y actualiza el efecto a un efecto custom que otorga inmunidad a la Fiebre del Bosque

--- src/main/java/com/crawkatt/meicamod/event/BiomeEvents.java
+++ src/main/java/com/crawkatt/meicamod/event/BiomeEvents.java
@@ -19,6 +19,12 @@ public class BiomeEvents implements ServerTickEvents.EndWorldTick {
             RegistryKey<Biome> currentBiome = world.getBiome(player.getBlockPos()).getKey().orElse(null);
             PlayerInfectionComponent infection = ModComponents.INFECTION.get(player);
 
+            if (player.hasStatusEffect(ModEffects.FOREST_BLESSING)) {
+                infection.setInfection(0);
+                player.removeStatusEffect(ModEffects.BROTIFICATION);
+                return;
+            }
+
             if (TARGET_BIOME.equals(currentBiome)) {
                 infection.addInfection(1, player);
                 player.addStatusEffect(new StatusEffectInstance(
