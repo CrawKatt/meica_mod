850d468ff1519056d824abf9ad4bd519af1f4dc2
CrawKatt <man849916@gmail.com>
2025-03-14 15:37:24 -0300

    Actualiza el Amplifier del efecto "Bendición del Bosque" al tener armadura completa

--- src/main/java/com/crawkatt/meicamod/item/custom/ModArmorItem.java
+++ src/main/java/com/crawkatt/meicamod/item/custom/ModArmorItem.java
@@ -16,7 +16,7 @@ import java.util.Map;
 public class ModArmorItem extends ArmorItem {
     public static final Map<ArmorMaterial, StatusEffectInstance> MATERIAL_TO_EFFECT_MAP =
             (new ImmutableMap.Builder<ArmorMaterial, StatusEffectInstance>())
-                    .put(ModArmorMaterials.BROTENITA, new StatusEffectInstance(ModEffects.FOREST_BLESSING, -1, 1,
+                    .put(ModArmorMaterials.BROTENITA, new StatusEffectInstance(ModEffects.FOREST_BLESSING, -1, 0,
                             false, false, true)).build();
 
     public ModArmorItem(ArmorMaterial material, Type type, Settings settings) {
