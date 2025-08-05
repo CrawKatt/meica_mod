7b7d50e4dd6e77f32c0243375bab62c29cf7d3c7
CrawKatt <man849916@gmail.com>
2025-08-04 21:55:32 -0400

    Agrega Facing a la estatua de Meica

--- src/main/java/com/crawkatt/meicamod/block/custom/MeicaStatueBlock.java
+++ src/main/java/com/crawkatt/meicamod/block/custom/MeicaStatueBlock.java
@@ -1,20 +1,40 @@
 package com.crawkatt.meicamod.block.custom;
 
 import com.crawkatt.meicamod.block.entity.MeicaStatueBlockEntity;
+import net.minecraft.block.Block;
 import net.minecraft.block.BlockState;
 import net.minecraft.block.BlockWithEntity;
+import net.minecraft.block.HorizontalFacingBlock;
 import net.minecraft.block.entity.BlockEntity;
+import net.minecraft.item.ItemPlacementContext;
+import net.minecraft.state.StateManager;
+import net.minecraft.state.property.DirectionProperty;
 import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.Direction;
 import org.jetbrains.annotations.Nullable;
 
 public class MeicaStatueBlock extends BlockWithEntity {
+    public static final DirectionProperty FACING = HorizontalFacingBlock.FACING;
+
     public MeicaStatueBlock(Settings settings) {
         super(settings);
+        setDefaultState(this.stateManager.getDefaultState().with(FACING, Direction.NORTH));
     }
 
     @Override
     @Nullable
     public BlockEntity createBlockEntity(BlockPos pos, BlockState state) {
         return new MeicaStatueBlockEntity(pos, state);
+    }
+
+    @Override
+    protected void appendProperties(StateManager.Builder<Block, BlockState> builder) {
+        builder.add(FACING);
+    }
+
+    @Override
+    public @Nullable BlockState getPlacementState(ItemPlacementContext ctx) {
+        Direction facing = ctx.getPlayer() == null ? Direction.NORTH : ctx.getPlayer().getHorizontalFacing().getOpposite();
+        return this.getDefaultState().with(FACING, facing);
     }
 }
